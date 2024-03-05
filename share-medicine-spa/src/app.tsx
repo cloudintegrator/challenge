b0305da36a2720ee31b7b9d17a951fe32dc1ebcf
Anupam Gogoi <c059237@yara.com>
2024-03-05 17:52:40 -0300

    initial

--- share-medicine-spa/src/app.tsx
+++ share-medicine-spa/src/app.tsx
@@ -1,31 +1,70 @@
-import { AuthProvider, useAuthContext } from "@asgardeo/auth-react";
-import React, { FunctionComponent, ReactElement } from "react";
-import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./app.css";
-import { default as authConfig } from "./config.json";
-import { ErrorBoundary } from "./error-boundary";
-import { HomePage, NotFoundPage } from "./pages";
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BasicUserInfo, useAuthContext } from "@asgardeo/auth-react";
 
-const AppContent: FunctionComponent = (): ReactElement => {
-    const { error } = useAuthContext();
+function App() {
+  const {
+    signIn,
+    signOut,
+    getAccessToken,
+    isAuthenticated,
+    getBasicUserInfo,
+    state,
+  } = useAuthContext();
+  const [isAuthLoading, setIsAuthLoading] = useState(false);
+  const [signedIn, setSignedIn] = useState(false);
+  const [user, setUser] = useState<BasicUserInfo | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
-    return (
-        <ErrorBoundary error={error}>
-            <Router>
-            <Routes>
-                <Route path="/" element={ <HomePage /> } />
-                <Route element={ <NotFoundPage /> } />
-            </Routes>
-        </Router>
-        </ErrorBoundary>
-    )
-};
+  useEffect(() => {
+    async function signInCheck() {
+      setIsAuthLoading(true);
+      await sleep(2000);
+      const isSignedIn = await isAuthenticated();
+      setSignedIn(isSignedIn);
+      setIsAuthLoading(false);
+      return isSignedIn;
+    }
+    signInCheck().then((res) => {
+      if (res) {
+        getUser();
+      } else {
+        console.log("User has not signed in");
+      }
+    });
+  }, []);
+  async function getUser() {
+    setIsLoading(true);
+    const userResponse = await getBasicUserInfo();
+    setUser(userResponse);
+    setIsLoading(false);
+  }
 
-const App = () => (
-    <AuthProvider config={authConfig}>
-        <AppContent />
-    </AuthProvider>
-);
+  const handleSignIn = async () => {
+    signIn()
+      .then(() => {
+        setSignedIn(true);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  if (isAuthLoading) {
+    return <div className="animate-spin h-5 w-5 text-white">.</div>;
+  }
 
-render((<App />), document.getElementById("root"));
+  if (!signedIn) {
+    return <button onClick={handleSignIn}>Login</button>;
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
